9acb535c77e096a74c689c4879f6804fd5377af6
Ksaltotun <romah28ius@gmail.com>
2019-06-05 14:27:00 +0300

    go

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-var x = 5;
-var y = 6;
-var c = String(x) + " " + String(y);
-alert(c);
+/*
+function ifSpam(str){
+  str = str.toLowerCase();
+  if( str.indexOf('viagra') > 0 || str.indexOf('xxx') > 0){
+    return true;
+  } else { 
+    return false
+  }
+}
+
+x = ifSpam('free xxxxx');
+alert(x); */
+
+function truncate(str, maxlength) {
+  var strBuf;
+  strBuf = str.slice(0, maxlength-3);
+  strBuf += "...";
+  return strBuf; 
+}
+
+alert(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
